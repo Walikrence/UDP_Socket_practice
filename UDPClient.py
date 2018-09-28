fa38b711c0caa2194c34d56946d2f23277602543
walikrence <walikrence@163.com>
2018-09-28 21:38:14 +0800

    dfasdfdsafsdaf

---
+++ UDPClient.py
@@ -0,0 +1,17 @@
+from socket import *
+serverName = '127.0.0.1'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:\n')
+clientSocket.sendto(message, (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+
+message = raw_input('Input lowercase sentence:\n')
+clientSocket.sendto(message, (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+
+
+
+clientSocket.close()
